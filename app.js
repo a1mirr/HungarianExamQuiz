df1ee7f770940b15580aa5afc03a8271be3b35b5
Zemchenkov Daniil <zemchenkov.d@promail366.com>
2026-02-18 15:35:54 +0100

    fix

--- app.js
+++ app.js
@@ -79,23 +79,16 @@ const state = {
     reportedProblems: [],
     mockExamMode: false,
     sessionAnswers: {} // Key: topicId_questionIndex, Value: { answered: boolean, correct: boolean, userAnswer: any }
-
-// == DOM Elements ==
-
-
+};
 
 // == Helper Functions ==
 function shuffleArray(array) {
-        const shuffled = [...array];
-for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-}
-return shuffled;
-}
-
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-return shuffled;
+    return shuffled;
 }
 
 // Fix Bug #13: Remove unused normalizeAnswer
